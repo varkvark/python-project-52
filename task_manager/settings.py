54ea1742e263f45e79471f8f36e4eace530cdd4f
Viktor Arkhipov <arkhipov.vd@gmail.com>
2026-01-10 00:37:33 +0300

    task_manager/settings.py changed debug and allowed hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,12 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     'webserver',
     'localhost',
     '127.0.0.1'
+    'python-project-52-dh79.onrender.com'
 ]
 
 
