6d6c5a9cea3bbd113b01fbccb86f8bc21bf23103
Viktor Arkhipov <arkhipov.vd@gmail.com>
2026-01-11 17:38:08 +0300

    task_manager/settings.py syntax error remove

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from django.utils.translation import gettext_lazy as _
 
 
 load_dotenv()
@@ -33,8 +34,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
     'webserver',
     'localhost',
-    '127.0.0.1'
-    'python-project-52-dh79.onrender.com'
+    '127.0.0.1',
+    'python-project-52-dh79.onrender.com',
 ]
 
 
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -120,6 +122,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+USE_TZ = True
+
+#LANGUAGES = [
+#    ("en", _("English")),
+#    ("ru", _("Russian")),
+#]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
