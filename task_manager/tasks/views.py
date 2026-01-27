8db30155de530fa2b6306b046b2b83a069851831
Viktor Arkhipov <arkhipov.vd@gmail.com>
2026-01-28 00:20:32 +0300

    linter fixing task_manager/tasks/views.py

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,5 +1,4 @@
 from django.contrib import messages
-from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
