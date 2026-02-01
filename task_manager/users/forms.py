531914bd97a99284f0af541c7b9688ebd96a2534
Viktor Arkhipov <arkhipov.vd@gmail.com>
2026-02-01 03:30:11 +0300

    task_manager/users/forms.py linter fix

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.utils.translation import gettext_lazy as _
+
 from task_manager.users.models import User
 
 
